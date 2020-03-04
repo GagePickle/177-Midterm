b26ebc10e3521b44c06774510d77590744381837
Gage Pickle <gagepickle@yahoo.com>
2020-03-04 17:20:56 -0600

    Add basic error checking and alerts when user enters invalid data

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 const items = {};
 
 items.name = prompt("Enter the name of the item.");
+while (items.name === "") {
+  alert("Please enter data for the item's name");
+  items.name = prompt("Enter the name of the item.");
+}
 items.price = Number(prompt("Enter the price of the item"));
+while (!items.price) {
+  alert("Please enter a number for the item's price");
+  items.price = Number(prompt("Enter the price of the item"));
+}
 
 alert(items.name + " is worth " + items.price + " Rubels");
