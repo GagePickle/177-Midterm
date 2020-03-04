e63b5ad899cd77d29f0a19fa74b9ea287c7147a8
Gage Pickle <gagepickle@yahoo.com>
2020-03-04 17:10:57 -0600

    Add item object and basic prompts to get the objects properties

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const items = {};
+
+items.name = prompt("Enter the name of the item.");
+items.price = Number(prompt("Enter the price of the item"));
+
+alert(items.name + " is worth " + items.price + " Rubels");
